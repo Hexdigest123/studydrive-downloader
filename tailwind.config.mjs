30aed266ec45769a544357698205238b56e3af6b
Hexdigest <pierre.maurice.m256@googlemail.com>
2024-01-06 22:08:10 +0100

    added images + configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#020203",
+        primary: "#0E1015",
+        second: "#22232B",
+        accent: "#213BF9",
+        primary_text: "#FFFFFF",
+        secondary_text: "rgba(255,255,255,0.6)",
+      },
+    },
+  },
+  plugins: [],
+};
